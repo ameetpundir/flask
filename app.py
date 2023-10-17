b3e0dddab57a14ea5354df2913c533b08ef12c38
Amit <ameetpundir@gmail.com>
2023-10-17 13:02:39 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)  # turn this file into a flask application
+
+
+@app.route("/")
+def index():
+    # name = request.args.get("name")
+    # return render_template("index.html", nameOfPerson=name)
+    return render_template("index.html")
+
+
+# @app.route("/greet") # for get request
+# def greet():
+#     name = request.args.get("name", "world")  # default value is "world"
+#     return render_template("greet.html", name=name)
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    name = request.form.get("name", "world")  # default value is "world"
+    return render_template("greet.html", name=name)
